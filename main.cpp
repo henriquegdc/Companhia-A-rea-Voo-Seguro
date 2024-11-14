5f4cabae99dff37e1de2fc0d0aefba9239c8143a
Ana Coimbra <129782530+ananinacoimbra@users.noreply.github.com>
2024-11-14 15:57:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+#include "class.h"
+#include "implement.cpp"
 
-using namespace std;
+int main(){
+    int escolha;
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+    switch (escolha){
+            case 1:
+            cadastrarPassageiro();
+        break;
+            case 2:
+            cadastrarTripulacao();
+        break;
+            case 3:
+            cadastrarVoo();
+        break;
+            case 4:
+            cadastrarAssento();
+        break;
+            case 5:
+            criarReserva();
+        break;
+            case 6:
+            cancelarReserva();
+        break;
+            case 7:
+            buscaPassageiro();
+        break;
+            case 8:
+            buscaTripulante();
+        break;
+            default:
+
+        break;
+        
+    }
+
 }
