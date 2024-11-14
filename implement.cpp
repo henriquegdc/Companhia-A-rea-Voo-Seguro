a14b9cc566212ce253f0564ce10868495a0680c5
Ana Coimbra <129782530+ananinacoimbra@users.noreply.github.com>
2024-11-14 15:58:19 -0300

    Add files via upload

---
+++ implement.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include "class.h"
+
+
+void cadastrarPassageiro(){
+
+}
+
+void cadastrarTripulacao(){
+
+}
+
+void cadastrarVoo(){
+
+}
+
+void cadastrarAssento(){
+
+}
+
+void criarReserva(){
+    
+}
+
+void cancelarReserva(){
+    
+}
+
+void buscaPassageiro(){
+
+}
+
+void buscaTripulante(){
+    
+}
+
